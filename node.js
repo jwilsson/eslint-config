1f86e0bd7e39f73b4d88ea2cd534385a06d490f6
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2017-06-12 20:26:51 +0200

    Remove no-buffer-constructor rule
    This is already covered by the node/no-deprecated-api rule

--- node.js
+++ node.js
@@ -12,7 +12,6 @@ module.exports = {
     "callback-return": "error",
     "global-require": "error",
     "handle-callback-err": "error",
-    "no-buffer-constructor": "error",
     "no-new-require": "error",
     "no-path-concat": "error",
     "no-process-exit": "error",
