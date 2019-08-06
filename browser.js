c5f003cdeecc31bed5bc2256d7ee906ba6d3c8a4
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2019-08-06 08:38:59 +0200

    Lint fixes

--- browser.js
+++ browser.js
@@ -1,15 +1,15 @@
 module.exports = {
-  "extends": [
-      "./defaults.js",
-  ],
-  "env": {
-    "browser": true,
-    "jquery": true,
-  },
-  "rules": {
-    "no-alert": "error",
-    "no-implicit-globals": "error",
-    "no-script-url": "error",
-    "strict": ["error", "safe"],
-  },
+    extends: [
+        './defaults.js',
+    ],
+    env: {
+        browser: true,
+        jquery: true,
+    },
+    rules: {
+        'no-alert': 'error',
+        'no-implicit-globals': 'error',
+        'no-script-url': 'error',
+        'strict': ['error', 'safe'],
+    },
 };
--- node.js
+++ node.js
@@ -1,23 +1,23 @@
 module.exports = {
-  "plugins": ["node"],
-  "extends": [
-      "./defaults.js",
-      "plugin:node/recommended",
-  ],
-  "env": {
-    "node": true,
-  },
-  "rules": {
-    "callback-return": "error",
-    "global-require": "error",
-    "handle-callback-err": "error",
-    "no-new-require": "error",
-    "no-path-concat": "error",
-    "no-underscore-dangle": ["error", {
-        "allow": ["__dirname", "__filename"],
-    }],
-    "strict": ["error", "global"],
+    plugins: ['node'],
+    extends: [
+        './defaults.js',
+        'plugin:node/recommended',
+    ],
+    env: {
+        node: true,
+    },
+    rules: {
+        'callback-return': 'error',
+        'global-require': 'error',
+        'handle-callback-err': 'error',
+        'no-new-require': 'error',
+        'no-path-concat': 'error',
+        'no-underscore-dangle': ['error', {
+            allow: ['__dirname', '__filename'],
+        }],
+        'strict': ['error', 'global'],
 
-    "node/exports-style": ["error", "module.exports"],
-  },
+        'node/exports-style': ['error', 'module.exports'],
+    },
 };
