7fc50954914885e19794885d4f7f7ba6aab70dc3
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2020-04-25 16:58:02 +0200

    Switch to non-deprecated Node rules from eslint-plugin-node

--- node.js
+++ node.js
@@ -8,16 +8,16 @@ module.exports = {
         node: true,
     },
     rules: {
-        'callback-return': 'error',
-        'global-require': 'error',
-        'handle-callback-err': 'error',
-        'no-new-require': 'error',
-        'no-path-concat': 'error',
         'no-underscore-dangle': ['error', {
             allow: ['__dirname', '__filename'],
         }],
         'strict': ['error', 'global'],
 
+        'node/callback-return': 'error',
         'node/exports-style': ['error', 'module.exports'],
+        'node/global-require': 'error',
+        'node/handle-callback-err': 'error',
+        'node/no-new-require': 'error',
+        'node/no-path-concat': 'error',
     },
 };
